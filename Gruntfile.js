32616ef18c4d9a6a69538a2498a46939f2d8b545
slopjong <git@slopjong.de>
2014-03-01 15:44:22 +0100

    Added grunt support to run a server and watch files

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+// Generated on 2014-02-08 using generator-angular 0.7.1
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-exec');
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+
+        // Project settings
+        yeoman: {
+            // configurable paths
+            app: 'jsoneditor',
+            dist: 'dist'
+        },
+
+        exec: {
+            jake: {
+                command: 'jake',
+                stdout: false,
+                stderr: false
+            }
+        },
+
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            js: {
+                files: ['<%= yeoman.app %>/js/{,*/}*.js'],
+                tasks: ['exec:jake'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '.tmp',
+                        './'
+                    ]
+                }
+            }
+        },
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [
+                    {
+                        dot: true,
+                        src: [
+                            '.tmp',
+                            '<%= yeoman.dist %>/*',
+                            '!<%= yeoman.dist %>/.git*'
+                        ]
+                    }
+                ]
+            },
+            server: '.tmp'
+        }
+    });
+
+    grunt.registerTask('serve', function (target) {
+        grunt.task.run([
+            'clean',
+            'connect:livereload',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('server', function () {
+        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+        grunt.task.run(['serve']);
+    });
+
+};
