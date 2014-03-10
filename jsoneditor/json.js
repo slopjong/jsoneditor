d0dcda2aa57378d4e69ecefcaef7e00433b93e0b
slopjong <git@slopjong.de>
2014-03-10 16:20:36 +0100

    Forgot to add index.html and using a full spaceapi example now

--- jsoneditor/json.js
+++ jsoneditor/json.js
@@ -1,46 +1,5 @@
-var json = {
-  "api": "0.13",
-  "contact": {
-    "twitter": "@example",
-    "email": "e@xample.com",
-    "irc": "irc://irc.freenode.net/example",
-    "ml": "public@lists.example.com",
-    "issue_mail": "ZUB4YW1wbGUuY29tCg=="
-  },
-  "cache": {
-    "schedule": "m.02"
-  },
-  "sensors": {
-    "temperature": [
-      {
-        "value": null,
-        "unit": "°C",
-        "location": "Roof"
-      },
-      {
-        "value": null,
-        "unit": "°De",
-        "location": "Lab"
-      }
-    ]
-  }
-};
-
 //var json = {
 //  "api": "0.13",
-//  "space": "The space name",
-//  "logo": "http://your-space.com/logo.png",
-//  "url": "http://example.com",
-//  "location": {
-//    "address": "see the documentation",
-//    "lon": 5.973817,
-//    "lat": 39.240431
-//  },
-//  "spacefed": {
-//    "spacenet": false,
-//    "spacesaml": false,
-//    "spacephone": false
-//  },
 //  "contact": {
 //    "twitter": "@example",
 //    "email": "e@xample.com",
@@ -48,15 +7,6 @@ var json = {
 //    "ml": "public@lists.example.com",
 //    "issue_mail": "ZUB4YW1wbGUuY29tCg=="
 //  },
-//  "issue_report_channels": [
-//    "issue_mail"
-//  ],
-//  "state": {
-//    "open": null
-//  },
-//  "projects": [
-//    "http://github.com/example"
-//  ],
 //  "cache": {
 //    "schedule": "m.02"
 //  },
@@ -75,3 +25,53 @@ var json = {
 //    ]
 //  }
 //};
+
+var json = {
+  "api": "0.13",
+  "space": "The space name",
+  "logo": "http://your-space.com/logo.png",
+  "url": "http://example.com",
+  "location": {
+    "address": "see the documentation",
+    "lon": 5.973817,
+    "lat": 39.240431
+  },
+  "spacefed": {
+    "spacenet": false,
+    "spacesaml": false,
+    "spacephone": false
+  },
+  "contact": {
+    "twitter": "@example",
+    "email": "e@xample.com",
+    "irc": "irc://irc.freenode.net/example",
+    "ml": "public@lists.example.com",
+    "issue_mail": "ZUB4YW1wbGUuY29tCg=="
+  },
+  "issue_report_channels": [
+    "issue_mail"
+  ],
+  "state": {
+    "open": null
+  },
+  "projects": [
+    "http://github.com/example"
+  ],
+  "cache": {
+    "schedule": "m.02"
+  },
+  "sensors": {
+    "temperature": [
+      {
+        "value": null,
+        "unit": "°C",
+        "location": "Roof"
+      },
+      {
+        "value": null,
+        "unit": "°De",
+        "location": "Lab"
+      }
+    ]
+  }
+};
